78a7ce88fb70c1f93f244252e55c02a0f3a7ad5b
firstChairCoder <md1qq7@gmail.com>
2023-01-11 16:16:08 +0100

    chore: basic stack demo

--- App.tsx
+++ App.tsx
@@ -1,33 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Navigation } from "react-native-navigation";
 
-export default function App() {
+export default function App(props) {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title="Push Settings Screen" color="#710ce3" />
-      <StatusBar style="auto" />
+      <Button
+        title="Push Settings Screen"
+        color="#710ce3"
+        onPress={() =>
+          Navigation.push(props.componentId, {
+            component: {
+              name: "Settings",
+              options: {
+                topBar: {
+                  title: {
+                    text: "Settings",
+                  },
+                },
+              },
+            },
+          })
+        }
+      />
     </View>
   );
 }
 
+// Settings screen declaration - this is the screen we'll be pushing into the stack
+export const SettingsScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "whitesmoke",
+      }}
+    >
+      <Text>Settings Screen</Text>
+    </View>
+  );
+};
+
 App.options = {
   topBar: {
     background: {
-      color: "#4d089a"
+      color: "#4d089a",
     },
     title: {
       text: "Home",
-      color: "white"
-    }
-  }
-}
+      color: "white",
+    },
+  },
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'whitesmoke',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "whitesmoke",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import { Navigation } from "react-native-navigation";
 
-import App from "./App";
+import App, { SettingsScreen } from "./App";
 
 
 
 Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
+Navigation.registerComponent("Settings", () => SettingsScreen)
+
+Navigation.setDefaultOptions({
+	statusBar: {
+		backgroundColor: "#4d089a"
+	},
+	topBar: {
+		backButton: {
+			color: "white"
+		},
+		background: {
+			color: "#4d089a"
+		},
+		title: {
+			color: "#FFF"
+		}
+	}
+})
 
 Navigation.events().registerAppLaunchedListener(() => {
    Navigation.setRoot({
